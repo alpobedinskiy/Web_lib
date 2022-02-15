8361493a0188c597b68a6571aa25c560c81b92d2
Aleksandr Pobedinskiy <pobedinskiy85@gmail.com>
2022-02-15 12:58:37 +0500

    les-15

---
+++ web_lib/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from web_lib.views import main, authors, books
+
+urlpatterns = [
+    path('', main, name='web_lib'),
+    path('authors', authors, name='authors'),
+    path('books', books, name='books'),
+]
